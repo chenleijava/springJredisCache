a3844efee2b92d3677f4650757f511a7a9f8ae23
石头哥哥 <502959937@qq.com>
2014-01-19 22:45:01 +0800

    添加JRedis 测试用例 junit

--- src/test/java/testJRedis/Test.java
+++ src/test/java/testJRedis/Test.java
@@ -1,7 +1,8 @@
 package testJRedis;
 
 import org.apache.log4j.xml.DOMConfigurator;
-import org.springframework.beans.factory.BeanFactory;
+import org.junit.After;
+import org.junit.Before;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Service;
 import springJredisCache.JRedisCache;
@@ -22,14 +23,18 @@ public class Test {
     @Resource
     private JRedisCache jRedisCache;
 
+   private  FileSystemXmlApplicationContext springContext ;
+    @Before
+    public void IntiRes(){
+        DOMConfigurator.configure("res/appConfig/log4j.xml");
+        System.setProperty("java.net.preferIPv4Stack", "true"); //Disable IPv6 in JVM
+        springContext = new FileSystemXmlApplicationContext("res/springConfig/spring-context.xml");
+    }
+
 
     @org.junit.Test
     public void  testJRedis(){
-        DOMConfigurator.configure("res/appConfig/log4j.xml");
-
-        System.setProperty("java.net.preferIPv4Stack", "true"); //Disable IPv6 in JVM
         /**初始化spring容器*/
-        BeanFactory springContext = new FileSystemXmlApplicationContext("res/springConfig/spring-context.xml");
         Test test= (Test) springContext.getBean("test");
         ArrayList<Integer> list=new ArrayList<Integer>();
         list.add(1);
@@ -40,11 +45,17 @@ public class Test {
         test.jRedisCache.putList("list", list);
 
         // get
-        list= (ArrayList<Integer>) test.jRedisCache.getList("list");
+        list= (ArrayList<Integer>)test. jRedisCache.getList("list");
 
         for(int value:list){
             System.out.println("get value from redis:" + value);
         }
+    }
+
+
+    @After
+    public void closeApp(){
+        springContext.close();
     }
 
 

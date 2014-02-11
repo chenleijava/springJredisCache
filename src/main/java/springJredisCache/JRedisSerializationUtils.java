ea357b3b9fd5fc284a547d94468728044218dda4
石头哥哥 <502959937@qq.com>
2014-02-11 13:42:52 +0800

    修改序列化方案  fstSerilization

--- src/main/java/springJredisCache/JRedisSerializationUtils.java
+++ src/main/java/springJredisCache/JRedisSerializationUtils.java
@@ -8,14 +8,22 @@
 package springJredisCache;
 
 
-import java.io.*;
+import de.ruedigermoeller.serialization.FSTObjectInput;
+import de.ruedigermoeller.serialization.FSTObjectOutput;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
 
 /**
  * @author 石头哥哥 </br>
  *         dcserver1.3 </br>
  *         Date:14-1-9 </br>
  *         Time:下午4:00 </br>
- *         Comment： 对象序列化工具类
+ *         Package:{@link springJredisCache}</br>
+ *         Comment： 对象序列化工具类    序列化方案基于 FST - Fast Serialization
+ *         https://github.com/flapdoodle-oss/de.flapdoodle.fast-serialization
  */
 public class JRedisSerializationUtils {
 
@@ -31,35 +39,8 @@ public class JRedisSerializationUtils {
         super();
     }
 
-
-    /**
-     * 将对象序列化
-     * @param obj
-     * @param outputStream
-     */
-    private static void serialize(Serializable obj, OutputStream outputStream) {
-        if (outputStream == null) {
-            throw new IllegalArgumentException("The OutputStream must not be null");
-        }
-        ObjectOutputStream out = null;
-        try {
-            // stream closed in the finally
-            out = new ObjectOutputStream(outputStream);
-            out.writeObject(obj);
-
-        } catch (IOException ex) {
-            throw new JRedisCacheException(ex);
-        } finally {
-            try {
-                if (out != null) {
-                    out.close();
-                }
-            } catch (IOException ex) {
-                // ignore close exception
-            }
-        }
-    }
-
+    // Serialize
+    //-----------------------------------------------------------------------
     /**
      * <p>Serializes an <code>Object</code> to a byte array for
      * storage/serialization.</p>
@@ -69,42 +50,51 @@ public class JRedisSerializationUtils {
      * @throws JRedisCacheException (runtime) if the serialization fails
      */
     public static byte[] serialize(Serializable obj) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream(512);
-        serialize(obj, baos);
-        return baos.toByteArray();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(512);
+        serialize(obj, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
     }
 
-
     /**
-     * 反序列化对象
-     * @param inputStream
-     * @return
+     * <p>Serializes an <code>Object</code> to the specified stream.</p>
+     *
+     * <p>The stream will be closed once the object is written.
+     * This avoids the need for a finally clause, and maybe also exception
+     * handling, in the application code.</p>
+     *
+     * <p>The stream passed in is not buffered internally within this method.
+     * This is the responsibility of your application if desired.</p>
+     *
+     * @param obj  the object to serialize to bytes, may be null
+     * @param byteArrayOutputStream  the stream to write to, must not be null
+     * @throws IllegalArgumentException if <code>outputStream</code> is <code>null</code>
+     * @throws org.apache.commons.lang.SerializationException (runtime) if the serialization fails
      */
-    private static Object deserialize(InputStream inputStream) {
-        if (inputStream == null) {
-            throw new IllegalArgumentException("The InputStream must not be null");
+    private static void serialize(Serializable obj, ByteArrayOutputStream byteArrayOutputStream) {
+        if (byteArrayOutputStream == null) {
+            throw new IllegalArgumentException("The OutputStream must not be null");
         }
-        ObjectInputStream in = null;
+        FSTObjectOutput out = null;
         try {
             // stream closed in the finally
-            in = new ObjectInputStream(inputStream);
-            return in.readObject();
-
-        } catch (ClassNotFoundException ex) {
-            throw new JRedisCacheException(ex);
+            out = new FSTObjectOutput(byteArrayOutputStream);
+            out.writeObject(obj);
+            out.flush();
         } catch (IOException ex) {
             throw new JRedisCacheException(ex);
         } finally {
             try {
-                if (in != null) {
-                    in.close();
+                if (out != null) {
+                    out.close();
+                    out=null;
                 }
             } catch (IOException ex) {
                 // ignore close exception
             }
         }
     }
-
+    // Deserialize
+    //-----------------------------------------------------------------------
     /**
      * <p>Deserializes a single <code>Object</code> from an array of bytes.</p>
      *
@@ -117,7 +107,46 @@ public class JRedisSerializationUtils {
         if (objectData == null) {
             throw new IllegalArgumentException("The byte[] must not be null");
         }
-        ByteArrayInputStream bais = new ByteArrayInputStream(objectData);
-        return deserialize(bais);
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(objectData);
+        return deserialize(byteArrayInputStream);
+    }
+    /**
+     * <p>Deserializes an <code>Object</code> from the specified stream.</p>
+     *
+     * <p>The stream will be closed once the object is written. This
+     * avoids the need for a finally clause, and maybe also exception
+     * handling, in the application code.</p>
+     *
+     * <p>The stream passed in is not buffered internally within this method.
+     * This is the responsibility of your application if desired.</p>
+     *
+     * @param byteArrayInputStream  the serialized object input stream, must not be null
+     * @return the deserialized object
+     * @throws IllegalArgumentException if <code>inputStream</code> is <code>null</code>
+     * @throws JRedisCacheException (runtime) if the serialization fails
+     */
+    private static Object deserialize(ByteArrayInputStream byteArrayInputStream) {
+        if (byteArrayInputStream == null) {
+            throw new IllegalArgumentException("The InputStream must not be null");
+        }
+        FSTObjectInput in = null;
+        try {
+            // stream closed in the finally
+            in = new FSTObjectInput(byteArrayInputStream);
+            return in.readObject();
+        } catch (ClassNotFoundException ex) {
+            throw new JRedisCacheException(ex);
+        } catch (IOException ex) {
+            throw new JRedisCacheException(ex);
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                    in=null;
+                }
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+        }
     }
 }
